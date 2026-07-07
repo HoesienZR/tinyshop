95e735f6e8b931314217f5f727b38b9f6b56faf3
Hossienzr110000 <Hossienzr110000@gmail.com>
2026-07-07 17:16:36 +0330

    fix(service) : finished todo and added typehint

--- tinyshop/services.py
+++ tinyshop/services.py
@@ -76,14 +76,11 @@ class CheckoutService:
                  order_repository:OrderRepository) -> None:
         self.cart_repository = cart_repository
         self.order_repository = order_repository
-    def checkout(self,cart_id:int,order_id) -> Order:
+    def checkout(self,cart_id:int,order_id:int ) -> Order:
         cart = self.cart_repository.get(cart_id=cart_id)
         if not cart.items:
             raise ValueError(f"No items in  found in cart with item {cart_id}")
-        order_items = []
-        #TODO fix the type hint below
-        for item in cart.items:
-            order_items.append(OrderItem.from_cart_item(item))
+        order_items = [ OrderItem.from_cart_item(item ) for item in cart.items ]
         order = Order(id=order_id,items= tuple(order_items))
         self.order_repository.add(order=order)
         self.cart_repository.remove(cart_id=cart_id)
